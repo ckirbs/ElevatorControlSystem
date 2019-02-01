0a1c01a303f96a391d2b4c78763266c129b35ced
brandonhartford <brandonhartford@CCSRDS02.CUNET.CARLETON.CA>
2019-02-01 13:52:25 -0500

    changes

--- ElevatorProject/src/newElevatorSystem/Temp.java
+++ ElevatorProject/src/newElevatorSystem/Temp.java
@@ -14,10 +14,11 @@ public class Temp {
 			DatagramPacket packetGet = new DatagramPacket(bufReceive, bufReceive.length);
 			DatagramSocket datagramSocketClient = new DatagramSocket();
 			DatagramPacket packetReceive;
-			
-			bufFromClient = new byte[] {4, 0, 7, 0};
-			packetReceive = new DatagramPacket(bufFromClient, bufFromClient.length, InetAddress.getByName("127.0.0.1"), 69);
-			datagramSocketClient.send(packetReceive);
+			for (int i = 9; i > -1; i--) {
+				bufFromClient = new byte[] {4, 0, (byte)i, 0};
+				packetReceive = new DatagramPacket(bufFromClient, bufFromClient.length, InetAddress.getByName("127.0.0.1"), 69);
+				datagramSocketClient.send(packetReceive);
+			}
 			
 			datagramSocketClient.receive(packetGet);
 			System.out.println(Arrays.toString(packetGet.getData()));
