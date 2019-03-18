387e1527362895c98ce5ad7f972dd1ef9790a44a
Darren Holden <darren.a.holden@outlook.com>
2019-03-18 19:43:01 -0400

    Removing magic strings

--- ElevatorProject/src/resources/SystemFile.java
+++ ElevatorProject/src/resources/SystemFile.java
@@ -89,14 +89,14 @@ public class SystemFile {
 
 			Directions direction;
 
-			if (messageDetails[2].equalsIgnoreCase("up")) {
+			if (messageDetails[2].equalsIgnoreCase(Directions.UP.toString())) {
 				direction = Directions.UP;
 
-			} else if (messageDetails[2].equalsIgnoreCase("down")) {
+			} else if (messageDetails[2].equalsIgnoreCase(Directions.DOWN.toString())) {
 				direction = Directions.DOWN;
-			} else if (messageDetails[2].equalsIgnoreCase("error_hard")) {
+			} else if (messageDetails[2].equalsIgnoreCase(Directions.ERROR_HARD.toString())) {
 				direction = Directions.ERROR_HARD;
-			} else if (messageDetails[2].equalsIgnoreCase("error_soft")) {
+			} else if (messageDetails[2].equalsIgnoreCase(Directions.ERROR_SOFT.toString())) {
 				direction = Directions.ERROR_SOFT;
 			} else // If not "up" or "down" it's an invalid direction
 			{
