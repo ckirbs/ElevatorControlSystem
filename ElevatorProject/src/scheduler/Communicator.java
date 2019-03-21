672d91b5c0a72ec84fdf16ff0a5a4e87d2c73558
Brandon <hartbrand99@gmail.com>
2019-03-20 22:24:22 -0400

    New GUI with some functionality

--- ElevatorProject/src/scheduler/Communicator.java
+++ ElevatorProject/src/scheduler/Communicator.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Date;
 
 import scheduler.Dispatcher;
@@ -23,24 +24,27 @@ import resources.Directions;
 public class Communicator {
 	protected static DatagramSocket floorSocket, elevatorSocket;
 	protected static Dispatcher dispatcher = new Dispatcher();
-	protected final int TIMEOUT_TIME = 1000;
+	protected final int TIMEOUT_TIME = 50;
 	protected static int elevatorReturnPort;
 	protected static int floorPort;
 	protected static ArrayList<ArrayList<Set<Integer>>> destinations = new ArrayList<ArrayList<Set<Integer>>>();
 	protected static ArrayList<byte[]> deniedReqs = new ArrayList<byte[]>();
-	private static ArrayList<byte[]> tempDeniedHolder = new ArrayList<byte[]>();
 	protected static ArrayList<byte[]> pendingReqs = new ArrayList<byte[]>();
 	private static int currReqId;
-	private static Set<Integer> pendingMandReqs = new HashSet<Integer>();
+	private static List<Floor> floors;
 	
 	public Communicator() {
 		Communicator.currReqId = 1;
+		floors = new ArrayList<Floor>();
+		for (int i = 0; i < NUMBER_OF_FLOORS; i++) {
+			floors.add(new Floor(i));
+		}
 	}
 	
 	/**
-	 * Takes in a message and performs the appropriate actions on it
+	 * Takes in a message and preforms the appropriate actions on it
 	 * 
-	 * @param message	The byte array of the message
+	 * @param message	The byte array of hte message
 	 * @return			True if properly handled, false otherwise
 	 */
 	public boolean handleNewMessage(byte[] message) {
@@ -55,13 +59,12 @@ public class Communicator {
 		case (byte) OPEN_CLOSE_DOOR: return this.openCloseDoor(flag, val1, val2, val3);
 		case (byte) CONFIRM_VOL_DESTINATION: return this.processConfirmation(flag, val1, val2, val3);
 		case (byte) STATUS_REPORT: return this.processStatusReport(flag, val1, val2);
-		case (byte) ERROR: return this.processErrorRequest(flag, val1, val2);
 		default: return false;
 		}
 	}
 
 	/**
-	 * Handles a status report from an elevator, and updates dispatcher
+	 * Handles a status report from an elvator, and updates dispatcher
 	 * 
 	 * @param dir			The elevator's direction
 	 * @param floorNum		The elevator's current floor
@@ -70,11 +73,6 @@ public class Communicator {
 	 */
 	private boolean processStatusReport(byte dir, byte floorNum, byte elevatorNum) {
 		System.out.println(FORMATTER.format(new Date()) + ": Updating status of elev " + (int) elevatorNum);
-		if (dir == (byte) Directions.getIntByDir(Directions.ERROR_HARD))  {
-			System.out.println(FORMATTER.format(new Date()) + ": Elevator " + (int)elevatorNum + " stuck permanently: Reschedule pending requests");
-			Thread reallocator = new Thread(new Reallocator((int) elevatorNum));
-			reallocator.start();
-		} 
 		return Communicator.dispatcher.updateElevatorInfo((int) elevatorNum, Directions.getDirByInt((int) dir) , (int) floorNum);
 	}
 
@@ -87,10 +85,8 @@ public class Communicator {
 	 * @return
 	 */
 	private boolean processConfirmation(byte yesNoVal, byte floorNum, byte elevatorNum, byte id) {
-		// Confirmation for mandatory requests - no action needed
 		if (id == (byte) 0) return true;
 		
-		// Find the corresponding pending request and remove from the list
 		byte[] req = null;
 		synchronized (pendingReqs) {
 			for (byte[] b: pendingReqs) {
@@ -102,29 +98,19 @@ public class Communicator {
 			pendingReqs.remove(req);
 		}
 		
-		// If the request was a mandatory destination
-		if (req == null && pendingMandReqs.contains((int)id)) {
-			pendingMandReqs.remove((int)id);
-			return true;
-		// If the request id is invalid
-		} else if (req == null) {
-			System.err.println(FORMATTER.format(new Date()) + ": Confirmation error for floor: " + floorNum + ", elevator: " + elevatorNum + ", request id: " + id);
-			return false;
-		}
+		//System.out.println(req);
 		
-		// If denied, add the request to the denied requests list
 		if (yesNoVal == NO) {
-			synchronized (Communicator.deniedReqs) {
-				Communicator.deniedReqs.add(new byte[] {req[1], req[2], req[3]});
+			synchronized (deniedReqs) {
+				deniedReqs.add(new byte[] {req[1], req[2], req[3]});
 			}
 			return false;
-			
-		// If accepted, add the new destinations to the elevators future destination set
 		} else {
 			synchronized (destinations) {
 				destinations.get((int) elevatorNum).get((int) floorNum).add((int) req[3]);
 			}
 		}
+		
 		return true;
 	}
 
@@ -145,13 +131,22 @@ public class Communicator {
 			msg[3] = elevatorNum;
 			msg[4] = dir;
 			
+			for (Floor floor : floors) {
+				if (floor.getLevel() == floorNum) {
+					if (openClose == OPEN) {
+						floor.isDoorOpen = true;
+					} else {
+						floor.isDoorOpen = false;
+					}
+					break;
+				}
+			}
+			
 			System.out.println(FORMATTER.format(new Date()) + ": " + ((openClose == OPEN) ? "Opening " : "Closing ") + "doors on floor " + (int) floorNum + " for elevator " + (int) elevatorNum);
 			
-			// Pass the message to the floor
 			DatagramPacket packet = new DatagramPacket(msg, MESSAGE_LENGTH, InetAddress.getByName(FLOOR_SYS_IP_ADDRESS), floorPort);
 			floorSocket.send(packet);
 			
-			// If it's an open request, check for any new destination (i.e. from passengers getting on)
 			if (openClose == OPEN) {
 				Set<Integer> tempSet;
 				synchronized (destinations) {
@@ -161,22 +156,15 @@ public class Communicator {
 				
 				System.out.println(FORMATTER.format(new Date()) + ": Sending elevator " + (int) elevatorNum + " new destinations: " + tempSet.toString());
 				
-				// Construct the generic message structure
 				DatagramPacket pckt;
 				byte[] destMsg = new byte[MESSAGE_LENGTH];
 				destMsg[0] = NEW_ELEVATOR_DESTINATION;
 				destMsg[1] = MANDATORY;
-				destMsg[3] = elevatorNum;
+				destMsg[3] = elevatorNum; // elevator number
+				destMsg[4] = (byte) 0; // Direction (not used atm)
 				
-				// For each new destination, send it to the elevator
 				for (int i: tempSet) {
 					destMsg[2] = (byte) i;
-					// Give id to mandatory request and increment it
-					destMsg[4] = (byte) currReqId;
-					pendingMandReqs.add(currReqId);
-					currReqId ++;
-					
-					// Send the packet
 					pckt = new DatagramPacket(destMsg, MESSAGE_LENGTH, InetAddress.getByName(ELEVATOR_SYS_IP_ADDRESS), ELEVATOR_PORT);
 					elevatorSocket.send(pckt);
 				}
@@ -201,7 +189,6 @@ public class Communicator {
 	 */
 	private boolean processNewRequest(byte dir, byte origFloor, byte destFloor) {
 		
-		// Create the request, giving it an id
 		byte[] message = new byte[MESSAGE_LENGTH];
 		message[0] = NEW_ELEVATOR_DESTINATION;
 		message[1] = VOLUNTARY;
@@ -210,20 +197,14 @@ public class Communicator {
 		message[5] = (byte) Communicator.currReqId;
 		Communicator.currReqId++;
 				
-		// Update the dispatcher information
 		Communicator.updateDispatcher();
 		
-		// Pick an elevator to send a request to
 		int elevatorNumber = Communicator.dispatcher.getNearestElevator(Directions.getDirByInt((int) dir), (int) origFloor);
 		
-		// If an elevator was chosen
 		if (elevatorNumber != -1) {
 			System.out.println(FORMATTER.format(new Date()) + ": Sending elevator " + elevatorNumber + " new destination: " + (int) origFloor + " Direction: " + Directions.getDirByInt((int) dir));
 			
-			// The request is now pending
 			pendingReqs.add(new byte[] {message[5], dir, origFloor, destFloor, (byte) elevatorNumber});
-			
-			// Send the request to the elevator
 			try {
 				message[3] = (byte) elevatorNumber;
 				DatagramPacket pckt = new DatagramPacket(message, MESSAGE_LENGTH, InetAddress.getByName(ELEVATOR_SYS_IP_ADDRESS), ELEVATOR_PORT);
@@ -233,79 +214,27 @@ public class Communicator {
 				return false;
 			}
 			return true;
-			
-		// If no elevator was chosen, consider the request denied
 		} else {
-			synchronized (Communicator.deniedReqs) {
-				Communicator.deniedReqs.add(new byte[] {dir, origFloor, destFloor});
-			}
 			return false;
 		}
 
 	}
 	
-	/**
-	 * Method to set elevators into "broken" states, where the doors are stuck open or the elevator 
-	 * is stuck between floors.
-	 * 
-	 * 
-	 * @param dir
-	 * @param elevatorNumber
-	 * @param timer (not mandatory, but for future consideration)
-	 * @return
-	 */
-	private boolean processErrorRequest(byte dir, byte elevatorNumber, byte timer) {
-		// Create the request, giving it an id
-		byte[] message = new byte[MESSAGE_LENGTH];
-		message[0] = ERROR;
-		message[1] = MANDATORY;
-		message[3] = elevatorNumber;
-		message[4] = dir;
-		message[5] = (byte) Communicator.currReqId;
-		Communicator.currReqId++;
-		
-		System.out.println(FORMATTER.format(new Date()) + ": Sending elevator " + elevatorNumber + " an error notice: " + Directions.getDirByInt((int) dir));
-		
-		// The request is now pending
-		pendingReqs.add(new byte[] {message[5], dir, elevatorNumber, timer, (byte) elevatorNumber});
-			
-		// Send the request to the elevator
-		try {
-			message[3] = (byte) elevatorNumber;
-			DatagramPacket pckt = new DatagramPacket(message, MESSAGE_LENGTH, InetAddress.getByName(ELEVATOR_SYS_IP_ADDRESS), ELEVATOR_PORT);
-			elevatorSocket.send(pckt);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-			
-		return true;
-	}
-	
-	/**
-	 * Sends a status request to each elevator so that the dispatcher can update its data.
-	 */
 	private static synchronized void updateDispatcher() {
 		DatagramPacket pckt;
 		
-		// For each elevator
 		for (int i = 0; i < NUMBER_OF_ELEVATORS; i++) {
-			// Don't check permanently stuck elevators
-			if (Dispatcher.getElevatorDirectionByElevatorNumber(i) != Directions.ERROR_HARD) {
-    			// Generate and send a status report request
-    			byte[] msg = new byte[] {ELEVATOR_INFO_REQUEST, (byte) 0, (byte) 0, (byte) i, (byte) 0, (byte) 0};
-    			try {
-    				pckt = new DatagramPacket(msg, MESSAGE_LENGTH, InetAddress.getByName(ELEVATOR_SYS_IP_ADDRESS), ELEVATOR_PORT);
-    				elevatorSocket.send(pckt);
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
+			byte[] msg = new byte[] {ELEVATOR_INFO_REQUEST, (byte) 0, (byte) 0, (byte) i, (byte) 0, (byte) 0};
+			try {
+				pckt = new DatagramPacket(msg, MESSAGE_LENGTH, InetAddress.getByName(ELEVATOR_SYS_IP_ADDRESS), ELEVATOR_PORT);
+				elevatorSocket.send(pckt);
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 		
-		// Sleep for a brief period to give time for responses
 		try {
-			Thread.sleep(500);
+			Thread.sleep(1000);
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
@@ -314,64 +243,48 @@ public class Communicator {
 	/**
 	 * Retry all of the voluntary requests that were previously denied
 	 */
-	protected synchronized void retryDeniedReqs() {
-		synchronized (Communicator.deniedReqs) {
-			for (byte[] x: Communicator.deniedReqs) Communicator.tempDeniedHolder.add(x);
-			
-			Communicator.deniedReqs.clear();
-			
-			for (byte[] req: Communicator.tempDeniedHolder) {
+	protected void retryDeniedReqs() {
+		synchronized (deniedReqs) {
+			for (byte[] req: deniedReqs) {
 				processNewRequest(req[0], req[1], req[2]);
 			}
-			
-			Communicator.tempDeniedHolder.clear();
+			deniedReqs.clear();
 		}
 	}
 	
-	/**
-	 * A class that will reallocate all of a give elevator's pending requests
-	 * 
-	 * @author Darren
-	 *
-	 */
-	private class Reallocator implements Runnable {
-		private int elev;
- 
-		/**
-		 * Construct the reallocator with the target elevator
-		 * 
-		 * @param elev
-		 */
-		public Reallocator(int elev) {
-			this.elev = elev;
-		}
-		
-		@Override
-		public void run() {
-			System.out.println(FORMATTER.format(new Date()) + ": Reallocating requests for elevator " + (int) elev);
-			
-			// Grab the list of requests for people who are waiting for the elevator at a given floor
-			// Create a instance copy and empty the list after copying and release the lock
-			ArrayList<Set<Integer>> pendingReqList;
-			synchronized (destinations) {
-				pendingReqList = destinations.get(elev);
-				destinations.set(elev, new ArrayList<Set<Integer>>());
-			}
-			
-			// For each of the floors, find all of the waiting requests that would be picked up by the elevator
-			for (int floorNum = 0; floorNum < pendingReqList.size(); floorNum++) {
-				// For each request waiting at the current floor, reprocess the request so it gets allocated to a working elevator
-				for (int dest: pendingReqList.get(floorNum)) {
-					// Debug line to see it cycling the pending requests
-					//System.err.println(FORMATTER.format(new Date()) + ": Retrying elevator " + elev + " pending req: " + floorNum + " to " + dest);
-					if (floorNum < dest) {
-						processNewRequest((byte) Directions.getIntByDir(Directions.UP), (byte) floorNum, (byte) dest);
-					} else {
-						processNewRequest((byte) Directions.getIntByDir(Directions.DOWN), (byte) floorNum, (byte) dest);
-					}
-				}
+	public Dispatcher getDispatcher() {
+		return dispatcher;
+	}
+	
+	public ArrayList<Integer> getFloorsWithOpenDoor(){
+		ArrayList<Integer> returnList = new ArrayList<>();
+		for (Floor floor : floors) {
+			if (floor.isDoorOpen) {
+				returnList.add(floor.getLevel());
 			}
 		}
+		return returnList;
+	}
+	
+	private class Floor {
 		
+		private int level;
+		private boolean isDoorOpen = false;
+		
+		public Floor(int level) {
+			this.level = level;
+		}
+		
+		public void setIsDoorOpen(boolean isDoorOpen) {
+			this.isDoorOpen = isDoorOpen;
+		}
+		
+		public boolean getIsDoorOpen() {
+			return isDoorOpen;
+		}
+		
+		public int getLevel() {
+			return level;
+		}
 	}
 }
--- ElevatorProject/src/scheduler/ElevatorGUI.java
+++ ElevatorProject/src/scheduler/ElevatorGUI.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -12,24 +13,35 @@ import javax.swing.WindowConstants;
 import static resources.Constants.HIGHEST_FLOOR;
 import static resources.Constants.NUMBER_OF_ELEVATORS;
 
-public class ElevatorGUI extends JPanel{
+public class ElevatorGUI extends JPanel implements Runnable{
 	
 	private static final int SIZE_X = 2000;
 	private static final int SIZE_Y = 1300;
 	private static final int HEIGHT_OF_FLOOR_SEPERATOR = 5;
-	private static final int HEIGHT_OF_FLOOR = 115;
-	private static final int HEIGHT_OF_ELEVATOR = 100;
-	private static final int WIDTH_OF_ELEVATOR = 100;
+	private static final int HEIGHT_OF_FLOOR = 90;
+	private static final int HEIGHT_OF_ELEVATOR = 75;
+	private static final int WIDTH_OF_ELEVATOR = 75;
+	private static final int HEIGHT_OF_TEXT = 10;
+	private static final int HEIGHT_OF_BUFFER = 10;
+	private static final int WIDTH_OF_BUFFER = 10;
 	private static final String DOOR_CLOSED_PATH = "src/Resources/closedFloors.jpg";
+	private static final String DOOR_OPEN_PATH = "src/Resources/openFloors.jpg";
 	
 	private JFrame frame;
 	private int x = 0;
 	
+	private ElevatorListener elevatorListener;
+	private FloorListener floorListener;
+	private Dispatcher dispatcher;
 	
 	Image doorsClosed;
+	Image doorsOpen;
 	
-	public ElevatorGUI() {
+	public ElevatorGUI(ElevatorListener elevatorListener, FloorListener floorListener) {
 		super();
+		this.elevatorListener = elevatorListener;
+		this.floorListener = floorListener;
+		dispatcher = this.elevatorListener.getDispatcher();
 		frame = new JFrame("Elevator System");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.add(this);
@@ -38,6 +50,7 @@ public class ElevatorGUI extends JPanel{
 		setDoubleBuffered(true);
 		try {
 			doorsClosed = ImageIO.read(new File(DOOR_CLOSED_PATH));
+			doorsOpen = ImageIO.read(new File(DOOR_OPEN_PATH));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,35 +59,41 @@ public class ElevatorGUI extends JPanel{
 	
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.fillRect(30, 5, WIDTH_OF_ELEVATOR, HEIGHT_OF_ELEVATOR);
+		ArrayList<Integer> floorWithOpenDoor = floorListener.getFloorsWithOpenDoor();
 		for (int i = 0; i < HIGHEST_FLOOR; i++) {
-			g.fillRect(0, HEIGHT_OF_FLOOR + (HEIGHT_OF_FLOOR * i), SIZE_X, HEIGHT_OF_FLOOR_SEPERATOR);
-			g.drawString("Floor " + (HIGHEST_FLOOR - i), SIZE_X - 100, (HEIGHT_OF_FLOOR * i) - 90);
-			for (int j = 0; j < NUMBER_OF_ELEVATORS; j++) {
-				g.drawImage(doorsClosed, ((SIZE_X/NUMBER_OF_ELEVATORS) * j) + (i % 2) * 200, (HEIGHT_OF_FLOOR * i) + 5, WIDTH_OF_ELEVATOR, HEIGHT_OF_ELEVATOR, this);
+			g.fillRect(0, ((HEIGHT_OF_FLOOR + HEIGHT_OF_TEXT) * i) + HEIGHT_OF_BUFFER + HEIGHT_OF_FLOOR, SIZE_X/2, HEIGHT_OF_FLOOR_SEPERATOR);
+			g.drawString("Floor " + (HIGHEST_FLOOR - i), 20, ((HEIGHT_OF_FLOOR + HEIGHT_OF_TEXT) * i) + HEIGHT_OF_BUFFER + 5);
+			if (floorWithOpenDoor.contains(i)) {
+				g.drawImage(doorsOpen, WIDTH_OF_BUFFER, ((HEIGHT_OF_FLOOR + HEIGHT_OF_TEXT) * i) + HEIGHT_OF_BUFFER + HEIGHT_OF_TEXT, WIDTH_OF_ELEVATOR, HEIGHT_OF_ELEVATOR, this);
+			} else {
+				g.drawImage(doorsClosed, WIDTH_OF_BUFFER, ((HEIGHT_OF_FLOOR + HEIGHT_OF_TEXT) * i) + HEIGHT_OF_BUFFER + HEIGHT_OF_TEXT, WIDTH_OF_ELEVATOR, HEIGHT_OF_ELEVATOR, this);
 			}
 		}
-		for (int i = 0; i < NUMBER_OF_ELEVATORS; i++) {
-			
-		}
-		g.drawString("Floor 0", SIZE_X - 100, (HEIGHT_OF_FLOOR * 10) - 90);
 	}
 	
-	public static void main (String args[]) {
-		ElevatorGUI gui = new ElevatorGUI();
+//	public static void main (String args[]) {
+//		ElevatorGUI gui = new ElevatorGUI();
+//		while(true) {
+//			gui.repaint();
+//			try {
+//				Thread.sleep(15);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//	}
+
+	@Override
+	public void run() {
 		while(true) {
-			gui.addX();
-			gui.repaint();
+			this.repaint();
 			try {
-				Thread.sleep(15);
+				Thread.sleep(5);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-	}
-	
-	public void addX() {
-		x++;
 	}
 }
