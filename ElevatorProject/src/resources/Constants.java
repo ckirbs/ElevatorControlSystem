a546a19ac1340c0f155b2e318f7e556183393928
CKirbs <callum.kirby@carleton.ca>
2019-03-21 13:42:02 -0400

    Updated Travel/Service Speeds, Max # floors, and Floor Requests file
    Travel Speeds updated to reflect real world values, max floors increased from
    10 to 22. Floor request file updated to include additonal requests, over a span of
    2 minutes, between all available floors

--- ElevatorProject/src/resources/Constants.java
+++ ElevatorProject/src/resources/Constants.java
@@ -21,10 +21,10 @@ public final class Constants {
 	public static final int MESSAGE_LENGTH = 6;
 	public static final int NUMBER_OF_ELEVATORS = 4;
 	public static final int LOWEST_FLOOR = 0;
-	public static final int HIGHEST_FLOOR = 10;
+	public static final int HIGHEST_FLOOR = 22;
 	public static final int NUMBER_OF_FLOORS = HIGHEST_FLOOR - LOWEST_FLOOR + 1;
-	public static final int ELEVATOR_TRAVEL_SPEED_MS = 1000;
-	public static final int ELEVATOR_STOP_TIME = 2000;
+	public static final int ELEVATOR_TRAVEL_SPEED_MS = 1500;
+	public static final int ELEVATOR_STOP_TIME = 1750;
 	
 	public static final String SCHED_IP_ADDRESS = "127.0.0.1";
 	public static final String FLOOR_SYS_IP_ADDRESS = "127.0.0.1";
